1d42dcd7ed64895df94d8b386a9fc946b600e02b
roze-2003 <106463091+roze-2003@users.noreply.github.com>
2022-09-29 21:41:34 +0600

    Update dslab1_task2.cpp

--- dslab1_task2.cpp
+++ dslab1_task2.cpp
@@ -1,29 +1,18 @@
+//Extract Even Odd Numbers From Given Array
 #include<iostream>
 using namespace std;
 
 int main()
 {
     int i;
-    int arr1[13];
-    cout<<"Enter the values of array: ";
-    for(int i=0; i<13; i++)
-    {
-        cin>>arr1[i];
-    }
-
+    int arr1[13]={1,2,3,4,5,6,7,8,9,10,11,12,13};
+    
     cout<<"Array: ";
     for(int i=0; i<13; i++)
     {
         cout<<arr1[i]<<"\t";
     }
     cout<<endl;
-        
-    cout<<"Reverse Array: ";
-    for(int i=12; i>=0; i--)
-    {
-        cout<<arr1[i]<<"\t";
-    }
-    cout<<"\n";
     cout<<"\nEven Numbers Are: ";
     for (i=0 ; i<13; i++)
     {
